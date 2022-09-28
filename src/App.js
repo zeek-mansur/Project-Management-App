2bb83cc6ec7a6b2f5f8368b12ee40832b8a1665a
Zeek <mzulkiful@yahoo.com>
2022-09-28 21:38:59 +0100

    Router and Pages Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+
+import { BrowserRouter,Route, Switch } from 'react-router-dom';
+// styles 
 import './App.css';
+
+// pages and componenets 
+
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Project from './pages/project/Project'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+       <div className='container'> 
+          <Switch>
+            <Route exact path = '/'>
+              <Dashboard />
+            </Route>
+            <Route exact path = '/create'>
+              <Create />
+            </Route>
+            <Route exact path = '/login'>
+              <Login />
+            </Route>
+            <Route exact path = '/signup'>
+              <Signup />
+            </Route>
+            <Route exact path = '/projects/:id'>
+              <Project />
+            </Route>
+          </Switch>
+       </div>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,11 @@
+import './Signup.css'
+
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <div>
+      Signup
+    </div>
+  )
+}
