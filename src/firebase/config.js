331e6d20b16cb8b21834737a32ae15b793f7a5b1
Zeek <mzulkiful@yahoo.com>
2022-09-28 20:28:31 +0100

    Firebase Project Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDeX5QLS3cHxaSnGBbTDtjytRBqtwJhyCI",
+    authDomain: "thecolabsite.firebaseapp.com",
+    projectId: "thecolabsite",
+    storageBucket: "thecolabsite.appspot.com",
+    messagingSenderId: "1057661338402",
+    appId: "1:1057661338402:web:74cd71524d5b66fc6a9b7b"
+  };
+
+
+  // init fire 
+
+  firebase.initializeApp(firebaseConfig)
+
+  // init services
+
+  const projectFirestore = firebase.firestore()
+  const projectAuth = firebase.auth()
+
+  // time stamp
+
+  const timestamp = firebase.firestore.Timestamp
+
+
+  export {projectFirestore, projectAuth, timestamp}
