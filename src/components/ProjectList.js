f9b3f6af652d207f51a6a0f24f2f02121c426c91
Zeek <mzulkiful@yahoo.com>
2022-09-30 18:04:47 +0100

    Project Grid Created

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,15 +1,30 @@
+import { Link } from 'react-router-dom'
+
 // styles 
 
 import './ProjectList.css'
 
 import React from 'react'
+import Avatar from './Avatar'
 
 export default function ProjectList({projects}) {
   return (
-    <div>
+    <div className='project-list'>
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map(project => (
-        <p key={project.id}>{project.name}</p>
+       <Link to={`/projects/${project.id}`} key={project.id}>
+            <h4>{project.name}</h4>
+            <p>Due by {project.dueDate.toDate().toDateString()}</p>
+            <div className='assigned-to'>
+                <ul>
+                {project.assignedUsersList.map(user => (
+                    <li key={user.photoURL}>
+                        <Avatar src= {user.photoURL} />
+                    </li>
+                ))}
+                </ul>
+            </div>
+       </Link>
       ))}
     </div>
   )
