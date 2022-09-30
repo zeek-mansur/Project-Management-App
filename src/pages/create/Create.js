e53c8b9f2ccccd4efa2d19baf545899c15b32477
Zeek <mzulkiful@yahoo.com>
2022-09-30 16:58:47 +0100

    Making a New Project

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -3,6 +3,8 @@ import './Create.css'
 import React, { useEffect, useState } from 'react'
 import Select from 'react-select'
 import { useCollection } from '../../hooks/useCollection'
+import { timestamp } from '../../firebase/config'
+import {useAuthContext} from '../../hooks/useAuthContext'
 
 const categories = [
   {value: 'development', label: 'Development'}, 
@@ -15,6 +17,7 @@ export default function Create() {
 
   const {documents} = useCollection('users')
 const[users, setUsers] = useState([])
+const {user} = useAuthContext()
 
   // form field values
 
@@ -54,7 +57,32 @@ const[users, setUsers] = useState([])
           return
         }
 
-      console.log(name, details, dueDate, category.value, assignedUsers)
+        const createdBy = {
+
+          displayName : user.displayName,
+          photoURL : user.photoURL,
+          id : user.uid
+        }
+
+        const assignedUsersList = assignedUsers.map((u) => {
+            return {
+              displayName: u.value.displayName,
+              photoURL : u.value.photoURL, 
+              id: u.value.id
+            }
+        })
+
+        const project = {
+          name,
+          details,
+          categories: category.value,
+          dueDate: timestamp.fromDate(new Date(dueDate)),
+          comments: [],
+          createdBy,
+          assignedUsersList
+        }
+
+      console.log(project)
   }
 
   return (
