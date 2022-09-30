6bcf3d236d83a1c8e4bb87caeed88b4a9800d23c
Zeek <mzulkiful@yahoo.com>
2022-09-30 17:33:47 +0100

    Fetching Projects to Dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,11 +1,22 @@
-import './Dashboard.css'
 
+import { useCollection } from '../../hooks/useCollection'
 import React from 'react'
 
+// styles
+import './Dashboard.css'
+import ProjectList from '../../components/ProjectList'
+
+
 export default function Dashboard() {
+
+  const {documents, error} = useCollection('projects')
+
+
   return (
     <div>
-      Dashboard
+      <h2 className='page-title'>Dashboard</h2>
+      {error && <p className='error'>{error}</p>}
+      {documents && <ProjectList projects ={documents}/>}
     </div>
   )
 }
