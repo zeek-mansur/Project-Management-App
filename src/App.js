a2f3a168c59dbe94a771e0c72d3ac51b227d510d
Zeek <mzulkiful@yahoo.com>
2022-09-30 08:24:46 +0100

    Auth Guards & Redirects Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter,Route, Switch } from 'react-router-dom';
+import { BrowserRouter,Route, Switch, Redirect } from 'react-router-dom';
 // styles 
 import './App.css';
 
@@ -12,34 +12,43 @@ import Signup from './pages/signup/Signup'
 import Project from './pages/project/Project'
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+
+  const {user, authIsReady} = useAuthContext()
   return (
     <div className="App">
       
-      <BrowserRouter>
+      {authIsReady && <BrowserRouter>
       <Sidebar />
        <div className='container'> 
         <Navbar />
           <Switch>
             <Route exact path = '/'>
-              <Dashboard />
+            {!user && <Redirect to='/login' />}
+             {user && <Dashboard />}
             </Route>
             <Route exact path = '/create'>
-              <Create />
+            {!user && <Redirect to='/login' />}
+             {user && <Create />}
             </Route>
             <Route exact path = '/login'>
-              <Login />
+              {user && <Redirect to='/' />}
+              {!user && <Login />}
             </Route>
             <Route exact path = '/signup'>
-              <Signup />
+              {user && <Redirect to='/' />}
+              {!user && <Signup />}
             </Route>
+            
             <Route exact path = '/projects/:id'>
-              <Project />
+              {!user && <Redirect to='/login' />}
+              {user && <Project />}
             </Route>
           </Switch>
        </div>
-      </BrowserRouter>
+      </BrowserRouter>}
     </div>
   );
 }
