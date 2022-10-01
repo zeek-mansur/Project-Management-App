f260c9afd0524ceb20d5e2740bb7c8b5fa2a1a5c
Zeek <mzulkiful@yahoo.com>
2022-10-01 07:19:37 +0100

    Listing Comments on Comments Section

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,6 +1,7 @@
 
 
 import React, { useState } from 'react'
+import Avatar from '../../components/Avatar'
 import { timestamp } from '../../firebase/config'
 import {useAuthContext} from '../../hooks/useAuthContext'
 import { useFirestore } from '../../hooks/useFirestore'
@@ -33,6 +34,22 @@ export default function ProjectComments({project}) {
     <div className='project-comments'>
       <h4>Project Comments</h4>
 
+        <ul>
+            {project.comments.length > 0 && project.comments.map(comment => (
+                <li key={comment.id}>
+                <div className='comment-author'>
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+                </div>
+                <div className='comment-date'>
+                    <p>date here</p>
+                </div>
+                <div className='comment-content'>
+                    <p>{comment.content}</p>    
+                </div>
+                </li>
+            ))}
+        </ul>
       <form className='add-comments' onSubmit={handleSubmit}>
         <label>
             <span>Add new comment:</span>
